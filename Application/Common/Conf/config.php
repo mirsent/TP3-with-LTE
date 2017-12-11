794da28a19076f2915a32134c50932c684f83a3b
mirse <mirsent@outlook.com>
2017-12-11 18:06:48 +0800

    sweetalert

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -5,6 +5,7 @@ return array(
     'LOAD_EXT_CONFIG'   => 'db,webconfig',                  // 加载扩展配置文件
     'TMPL_PARSE_STRING' => array(                           // 模板相关配置
         '__PUBLIC__'    => __ROOT__.'/Public',
+        '__BOWER__'     => __ROOT__.'/Public/bower_components',
         '__STATICS__'   => __ROOT__.'/Public/statics',
         '__Home_CSS__'  => __ROOT__.trim(TMPL_PATH,'.').'/Home/Public/css',
         '__Home_JS__'   => __ROOT__.trim(TMPL_PATH,'.').'/Home/Public/js',
